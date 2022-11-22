d836b6ab80dc87c3031ba04b6a7e7473de750667
Lucas Schoenardie <lucas@Camilas-MacBook-Pro.local>
2022-11-22 10:39:50 -0500

    redirect root

--- next.config.js
+++ next.config.js
@@ -2,6 +2,26 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// module.exports = () => {
+//   const rewrites = () => {
+//     return [
+//       {
+//         source: "/cats",
+//         destination: "https://meowfacts.herokuapp.com",
+//       },
+//       {
+//         source: "/",
+//         destination: "https://avetti-subsite.netlify.app",
+//       },
+//     ];
+//   };
+//   return {
+//     rewrites,
+//     reactStrictMode: true,
+//     swcMinify: true,
+//   };
+// };
